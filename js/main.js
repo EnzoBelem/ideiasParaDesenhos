cd5461ea9f2457ec4f606b728844b93243ece4f0
Enzo Belem <enzogt5000@gmail.com>
2022-06-02 03:30:20 -0300

    new animations inserted

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 document.querySelector('#btn_sugestion').addEventListener('click', async () => {
-    document.querySelector('#sugestion_data').innerHTML= await getSugestion()
+    let sugestion= document.querySelector('#sugestion_data')
+    sugestionAnimationHidden(sugestion)
+    sugestion.innerHTML= await getSugestion()
+    sugestionAnimationShow(sugestion)
+    setTimeout( ()=>{
+        sugestionAnimation(sugestion)
+    }, 210)
 })
+
+function sugestionAnimation(sugestion){
+    sugestion.style.animation= "sugestionAnimation 5s linear infinite"
+}
+
+function sugestionAnimationShow(sugestion){
+    sugestion.style.animation= "sugestionAnimationShow 200ms linear 0s 1 normal forwards"
+}
+
+function sugestionAnimationHidden(sugestion){
+    sugestion.style.animation= "sugestionAnimationHidden 200ms linear 0s 1 normal forwards"
+}
 
 async function getSugestion(){
     let data
-
     await fetch('https://random-word-api.herokuapp.com/word')
     .then(response=> response.json())
     .then(word=> data= word[0])
-    
     return data
 }
